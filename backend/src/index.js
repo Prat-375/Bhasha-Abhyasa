28900f05834d301b44e3585bfb18fa0e5b444dba
Prateek Kumar Nayak <prateek.nayak50@gmail.com>
2026-03-29 16:10:14 +0200

    added vercel link

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-vercel-app.vercel.app",
+      "https://bhasha-abhyasa.vercel.app",
     ],
     credentials: true,
   })
